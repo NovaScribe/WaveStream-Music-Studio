abdb8fe1759ef6ff10eb1cd9d108f7d9a6437834
NovaScribe <maneeshbabu6@gmail.com>
2025-07-03 21:18:52 +0530

    03-07-2025

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 window.addEventListener('load', function () {
     document.body.style.display = 'block';
     addFooter();
+    // addoptions();
 });
 
 function performSearch() {
@@ -18,10 +19,50 @@ function addFooter() {
     footer.innerHTML = `
         <div class="footer-links">
             <a href="about.html">About</a>
-            <a href="https://www.instagram.com/m_a_n_e_sh_" target="_blank">Instagram</a>
+            <a href="https://www.instagram.com/vocal.hub.cloud/" target="_blank">Instagram</a>
             <a href="Privacy-Policy.html">Privacy Policy</a>
         </div>
         <div class="copyright">&copy; 2025 Vocal-Hub. All rights reserved.</div>`;
     document.body.appendChild(footer);
+}
+
+
+function addoptions() {
+    var nav = document.querySelector('nav');
+
+    // Create options container
+    var optionsDiv = document.createElement('div');
+    optionsDiv.className = 'options';
+    optionsDiv.innerHTML = `
+        <a href="#">option 1</a>
+        <a href="#">option 2</a>
+        <a href="#">option 3</a>
+    `;
+
+    // Create search bar
+    var searchBar = document.createElement('div');
+    searchBar.className = 'search-bar';
+    searchBar.innerHTML = `
+        <input type="text" id="searchQuery" placeholder="Search Here!">
+        <button onclick="performSearch()">Search</button>
+    `;
+
+    // Create mobile menu container
+    var mobileMenu = document.createElement('div');
+    mobileMenu.className = 'mobile-menu-container';
+    mobileMenu.innerHTML = `
+        <div class="mobile-menu-icon" onclick="toggleMobileMenu()">&#9776;</div>
+        <div class="mobile-options">
+            <a onclick="scrollm(this)" id="tutorial">Tutorials</a>
+            <a href="#">option 1</a>
+            <a href="#">option 2</a>
+            <a href="#">option 3</a>
+        </div>
+    `;
+
+    // Append to nav
+    nav.appendChild(optionsDiv);
+    nav.appendChild(searchBar);
+    nav.appendChild(mobileMenu);
 }
 

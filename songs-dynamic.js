61d06fe1bde1ca290affc03f658319a344bd3a0b
NovaScribe <maneeshbabu6@gmail.com>
2025-07-04 00:32:46 +0530

    03-07-2025 last

--- songs-dynamic.js
+++ songs-dynamic.js
@@ -52,7 +52,8 @@ var songs = [
         artist: 'Jakes Bejoy, Sid Sriram, and Sithara',
         cover: 'coverphoto/Minnalvala-500x500.jpg',
         page: 'minnalvala.html'
-    },{
+    },
+    {
         title: 'Bavra-Mann - Karaoke',
         year: '-',
         language: 'Malayalam',
@@ -60,6 +61,123 @@ var songs = [
         artist: '-',
         cover: 'coverphoto/bavra-mann.jpg',
         page: '/Bavra-Mann-Mayaandhi.html'
+    },
+    {
+        title: 'Uyire - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Gauthamante Radham',
+        artist: '-',
+        cover: 'coverphoto/uyire.jpg',
+        page: '/Uyire-karaoke.html'
+    },
+    {
+        title: 'Njan Jackson allada - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Ambili',
+        artist: '-',
+        cover: 'coverphoto/njan-jackson-allada.jpg',
+        page: '/njan-allada-ambili.html'
+    },
+    {
+        title: 'Mizhiyil Ninnum - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: '-',
+        artist: '-',
+        cover: 'coverphoto/mizhiyil-nunnum.jpg',
+        page: '/Oru-Deivam-Thantha.html'
+    },
+    {
+        title: 'Puthiyoru Pathayil - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: '-',
+        artist: '-',
+        cover: 'coverphoto/putiyoru-pathayil.jpg',
+        page: '/putiyoru-pathayil-karaoke.html'
+    },
+    {
+        title: 'Ezhutha Kadha - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Kumbalangi Nights',
+        artist: '-',
+        cover: 'coverphoto/ezhutha-kadha.jpg',
+        page: '/ezhutha-kadha-karaoke.html'
+    },
+    {
+        title: 'Koodu Vittu - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'June',
+        artist: '-',
+        cover: 'coverphoto/koodu-vittu.jpg',
+        page: '/koodu-vittu-karaoke.html'
+    },
+    {
+        title: 'Melle Enne.. Nooru.. - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Mandharam',
+        artist: '-',
+        cover: 'coverphoto/Melle-enne.jpg',
+        page: '/Melle-enne-karaoke.html'
+    },
+    {
+        title: 'Oduvile Theeyayi - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Varathan',
+        artist: '-',
+        cover: 'coverphoto/Oduvile-Theeyayi.jpg',
+        page: '/Oduvile-Theeyayi-karaoke.html'
+    },
+    {
+        title: 'Pulari mazhakal-Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Mandharam',
+        artist: '-',
+        cover: 'coverphoto/Pulari-mazhakal.jpg',
+        page: '/Pulari-mazhakal-karaoke.html'
+    },
+    {
+        title: 'Nee.. Pranayamothum-Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Varathan',
+        artist: '-',
+        cover: 'coverphoto/nee-pranayamodum.jpg',
+        page: '/nee-pranayamodum-karaoke.html'
+    },
+    {
+        title: 'Maanathe Kanalaali - Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Theevandi',
+        artist: '-',
+        cover: 'coverphoto/Maanthe-kanalaali.jpg',
+        page: '/Maanthe-kanalaali-karaoke.html'
+    },
+    {
+        title: 'ആനന്ദാമേ സർവ്വമാനന്ദാമേ- Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Aravindante Athidhikal',
+        artist: '-',
+        cover: 'coverphoto/poramo.jpg',
+        page: '/aanandhame-sarvam-karaoke.html'
+    },
+    {
+        title: 'Neramai Nilaa..Karaoke-Karaoke No-CopyRight',
+        year: '-',
+        language: 'Malayalam',
+        movie: 'Poomaram',
+        artist: '-',
+        cover: 'coverphoto/neramai-nelaa.jpg',
+        page: '/neramai-nelaa-karaoke.html'
     }
 ];
 
